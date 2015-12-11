2b7bce1ca59e7dddba246ce41e8a4c44801cca81
A-Clegg <itsammon@gmail.com>
2015-12-11 08:58:54 -0700

    Updated savedSearches partial to better handle newer code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -116,7 +116,8 @@ router.post('/search', function(req, res) {
     // console.log(feed)
     res.render('search', {
       layout: 'auth_base',
-      feed: feed.data
+      feed: feed.data,
+      search: req.body.search
     })
   })
 })
